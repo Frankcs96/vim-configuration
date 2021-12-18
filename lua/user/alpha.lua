a1da22bfb503bc48b3c0e18a44f73830b348837d
Francisco Carmona <francisco.carmona@springworks.se>
2021-12-18 16:23:38 +0100

    Feat: change config to full lua!

---
+++ lua/user/alpha.lua
@@ -0,0 +1,56 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
+
+local function get_splash()
+
+local file = io.open("/Users/francisco/.config/nvim/splashes.txt", "r");
+local size = 0
+local arr = {}
+math.randomseed(os.time())
+
+ for line in file:lines() do
+   size = size + 1
+    table.insert(arr, line);
+ end
+
+local random = math.random(1,size)
+
+return arr[random]
+ end
+
+local splash = get_splash()
+
+
+local dashboard = require("alpha.themes.dashboard")
+dashboard.section.header.val = {
+  [[                                                 ]],
+  [[                                                 ]],
+	[[███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+	[[████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+	[[██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+	[[██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+	[[██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+	[[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+	[[                                                  ]],
+}
+dashboard.section.buttons.val = {
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+dashboard.section.footer.val = splash
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.section.header.opts.position = "center"
+
+dashboard.opts.opts.noautocmd = true
+-- vim.cmd([[autocmd User AlphaReady echo 'ready']])
+alpha.setup(dashboard.opts)
