5c76f4ffdc521532cf859812ce1eb91ef79f5b58
Francisco Carmona <francisco.carmona@springworks.se>
2022-02-26 12:46:44 +0100

    feat: diable pyright type hint checker

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -25,6 +25,5 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		diagnostics.flake8,
 	},
 })
