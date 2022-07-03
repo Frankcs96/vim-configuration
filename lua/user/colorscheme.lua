5e8f864103b4d4f32fb5a459d570de3882475112
frankcs96 <fcsuarez96@gmail.com>
2022-07-03 11:13:16 +0200

    feat(colorscheme): use colorscheme settings

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,73 @@
 require("catppuccin").setup({
-	transparent_background = true,
+	styles = {
+		comments = "italic",
+		conditionals = "italic",
+		loops = "NONE",
+		functions = "NONE",
+		keywords = "italic",
+		strings = "NONE",
+		variables = "NONE",
+		numbers = "NONE",
+		booleans = "NONE",
+		properties = "NONE",
+		types = "NONE",
+		operators = "NONE",
+	},
+	integrations = {
+		treesitter = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = "italic",
+				hints = "italic",
+				warnings = "italic",
+				information = "italic",
+			},
+			underlines = {
+				errors = "underline",
+				hints = "underline",
+				warnings = "underline",
+				information = "underline",
+			},
+		},
+		coc_nvim = false,
+		lsp_trouble = false,
+		cmp = true,
+		lsp_saga = false,
+		gitgutter = false,
+		gitsigns = true,
+		telescope = true,
+		nvimtree = {
+			enabled = true,
+			show_root = true,
+			transparent_panel = true,
+		},
+		neotree = {
+			enabled = false,
+			show_root = false,
+			transparent_panel = false,
+		},
+		which_key = false,
+		indent_blankline = {
+			enabled = true,
+			colored_indent_levels = false,
+		},
+		dashboard = true,
+		neogit = false,
+		vim_sneak = false,
+		fern = false,
+		barbar = false,
+		bufferline = true,
+		markdown = true,
+		lightspeed = false,
+		ts_rainbow = false,
+		hop = false,
+		notify = true,
+		telekasten = true,
+		symbols_outline = true,
+	},
 })
 vim.cmd([[set termguicolors]])
 vim.cmd([[colorscheme catppuccin]])
 vim.cmd([[hi CursorLine guibg=none]])
 vim.cmd([[hi CursorLineNr guifg=#F5C2E7]])
-vim.cmd([[hi NvimTreeNormal guibg=none]])
-vim.cmd([[hi NvimTreeVertSplit guibg=none]])
