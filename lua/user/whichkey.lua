f5a24ecca77fa4230a04e2779dac023f7dc5e39f
Frankcs96 <fcsuarez96@gmail.com>
2023-08-22 11:32:07 +0200

    chore(project.nvim): remove pluging

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -110,7 +110,6 @@ local mappings = {
     "Find files",
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
-  ["P"] = { "<cmd>Telescope projects<cr>", "Projects" },
 
   p = {
     name = "Packer",
