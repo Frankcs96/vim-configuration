a430ca23f04d0522960769ce58eb4eb2613c5403
Frankcs96 <fcsuarez96@gmail.com>
2025-06-04 11:09:18 +0200

    chore: close oil with q like a popup

--- lua/fcscode/core/autocommands.lua
+++ lua/fcscode/core/autocommands.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "oil" },
   callback = function()
     vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR> 
